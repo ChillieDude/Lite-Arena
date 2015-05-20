d91b07668a2bd1cabd9c322f7fc57add13c897e4
ChillieDude <chillieman224@aim.com>
2015-05-20 05:23:03 -0400

    Testing Deconstructor
    Working

--- Bullet.cpp
+++ Bullet.cpp
@@ -1,12 +1,19 @@
 #include "Bullet.h"
 #include <iostream>
 
-Bullet::Bullet(const int uid, SDL_Rect entity, SDL_Color color, std::vector<Entity*>& entities, float speed, SDL_Rect target, const int SCREEN_W, const int SCREEN_H) : Entity(uid, entity, color, entities, speed), target(target), SCREEN_W(SCREEN_W), SCREEN_H(SCREEN_H){}
+Bullet::Bullet(const int uid, SDL_Rect entity, SDL_Color color, std::vector<Entity*>& entities, float speed, SDL_Rect target) : Entity(uid, entity, color, entities, speed), target(target)
+{
+    timer = SDL_GetTicks();
+}
 
 Bullet::~Bullet()
 {
     std::cout << "Bullet Destoryed" <<std::endl;
 }
 void Bullet::tick(float delta) {
-
+    int currentTime = SDL_GetTicks();
+    if(currentTime - timer > 500) //If the bullet has been alive for 0.5 seconds...
+    {
+        setAlive(false);
+    }
 }
