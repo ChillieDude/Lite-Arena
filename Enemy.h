b9f53f93b97ad78e70fbbf707b40cbd43e9fe1c7
ChillieDude <chillieman224@aim.com>
2015-05-20 04:42:33 -0400

    Update Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -6,6 +6,7 @@
 class Enemy : public Entity {
 public:
 	Enemy(const int uid, SDL_Rect entity, SDL_Color color, std::vector<Entity*>& entities, float speed);
+	~Enemy();
 	void tick(float delta);
 private:
 
