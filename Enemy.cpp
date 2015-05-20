32d591da978d96f679f034ea3edcad100d0b6a9c
ChillieDude <chillieman224@aim.com>
2015-05-20 04:49:58 -0400

    Update Enemy.cpp

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,6 +1,12 @@
 #include "Enemy.h"
+#include <iostream>
 
 Enemy::Enemy(const int uid, SDL_Rect entity, SDL_Color color, std::vector<Entity*>& entities, float speed) : Entity(uid, entity, color, entities, speed) {}
+
+Enemy::~Enemy()
+{
+  std::cout << "Enemy Destroyed" <<std::endl;
+}
 
 void Enemy::tick(float delta) {
 
