4122c60b1a44d1417a438c421a36509cf025dcb7
ChillieDude <chillieman224@aim.com>
2015-05-20 04:49:02 -0400

    Update Bullet.cpp

--- Bullet.cpp
+++ Bullet.cpp
@@ -1,7 +1,12 @@
 #include "Bullet.h"
+#include <iostream>
 
 Bullet::Bullet(const int uid, SDL_Rect entity, SDL_Color color, std::vector<Entity*>& entities, float speed, SDL_Rect target, const int SCREEN_W, const int SCREEN_H) : Entity(uid, entity, color, entities, speed), target(target), SCREEN_W(SCREEN_W), SCREEN_H(SCREEN_H){}
 
+Bullet::~Bullet()
+{
+    std::cout << "Bullet Destoryed" <<std::endl;
+}
 void Bullet::tick(float delta) {
 
 }
