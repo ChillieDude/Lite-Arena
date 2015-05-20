fdd0db8b20511e1071fcebc3ff59161d8ef23804
ChillieDude <chillieman224@aim.com>
2015-05-20 04:40:20 -0400

    Update Bullet.h

--- Bullet.h
+++ Bullet.h
@@ -5,10 +5,10 @@
 
 class Bullet : public Entity {
 public:
-	Bullet(const int uid, SDL_Rect entity, SDL_Color color, std::vector<Entity*>& entities, float speed, SDL_Rect target, const int SCREEN_WIDTH, const int SCREEN_HEIGHT);
+	Bullet(const int uid, SDL_Rect entity, SDL_Color color, std::vector<Entity*>& entities, float speed, SDL_Rect target);
+	~Bullet();
 	void tick(float delta);
 private:
-	const int SCREEN_W, SCREEN_H;
 	SDL_Rect target;
 };
 
