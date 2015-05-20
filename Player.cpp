5d89c0253e429d2ab915c353ec4a3c29b36c6d41
ChillieDude <chillieman224@aim.com>
2015-05-20 04:54:32 -0400

    Update Player.cpp
    Add HandleState - Utilized Tick function

--- Player.cpp
+++ Player.cpp
@@ -1,14 +1,41 @@
 #include "Player.h"
 #include "Enemy.h"
 #include <iostream>
+#include "Game.h"
 
 Player::Player(const int uid, SDL_Rect entity, SDL_Color color, std::vector<Entity*>& entities, float speed) : Entity(uid, entity, color, entities, speed) {}
 
-void Player::tick(float delta) {
-
+Player::~Player()
+{
+    std::cout << "Player destroyed: "<<std::endl;
 }
 
-void Player::move(int dirX, int dirY, float delta, const int SCREEN_W, const int SCREEN_H) {
+void Player::tick(float delta)
+{
+    move(xMove, yMove, delta);
+}
+
+void Player::HandleState(const Uint8* state)
+{
+    yMove = 0, xMove = 0;
+    if(state[SDL_SCANCODE_W])
+    {
+        yMove--;
+    }
+    if(state[SDL_SCANCODE_S])
+    {
+        yMove++;
+    }
+    if(state[SDL_SCANCODE_A])
+    {
+        xMove--;
+    }
+    if(state[SDL_SCANCODE_D])
+    {
+        xMove++;
+    }
+}
+void Player::move(int dirX, int dirY, float delta) {
 	SDL_Rect entity = getEntity();
 	int new_x = entity.x + ((dirX * (getSpeed() * delta)) + (dirX > 0 ? 1 : 0));
 	int new_y = entity.y + ((dirY * (getSpeed() * delta)) + (dirY > 0 ? 1 : 0));
@@ -31,9 +58,9 @@ void Player::move(int dirX, int dirY, float delta, const int SCREEN_W, const int
 	}
 
 	if(new_x < 0) new_x = 0;
-	if(new_x + next.w > SCREEN_W) new_x = SCREEN_W - next.w;
+	if(new_x + next.w > Game::SCREEN_WIDTH) new_x = Game::SCREEN_WIDTH - next.w;
 	if(new_y < 0) new_y = 0;
-	if(new_y + next.h > SCREEN_H) new_y = SCREEN_H - next.h;
+	if(new_y + next.h > Game::SCREEN_HEIGHT) new_y = Game::SCREEN_HEIGHT - next.h;
 
 	setPosition(new_x, new_y);
 }
